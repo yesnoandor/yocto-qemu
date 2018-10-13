59706ccb3f758a942519046df40e3ee49d2c5404
wenyu xu <wellok@139.com>
2018-10-13 11:01:01 +0800

    add simple test app to test hello-mod driver

--- poky/meta-yocto-avs/recipes-kernel/hello-mod/files/hello.c
+++ poky/meta-yocto-avs/recipes-kernel/hello-mod/files/hello.c
@@ -27,7 +27,7 @@
 #include <linux/sched.h>
 #include <asm/io.h>
 //#include <asm/system.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/device.h>
 
 #define simple_MAJOR 200  
@@ -37,67 +37,76 @@ static char demoBuffer[256];
 
 int simple_open(struct inode *inode,struct file *filp)
 {
-    if(simple_inc>0) return -ERESTARTSYS;
-    simple_inc++;
-    memset(demoBuffer,0,256);
-    return 0;
+	if(simple_inc>0) return -ERESTARTSYS;
+
+	simple_inc++;
+	memset(demoBuffer,0,256);
+
+	return 0;
 }
+
 int simple_release(struct inode *inode,struct file *filp)
 {
-        if(simple_inc<=0){
-                printk("close error");
-                return -EFAULT;
-        }
-        simple_inc--;
-    return 0;
+	if(simple_inc<=0){
+		printk("close error");
+		return -EFAULT;
+	}
+
+	simple_inc--;
+
+	return 0;
 }
+
 ssize_t simple_read(struct file *filp,char __user *buf,size_t count,loff_t *f_pos)
 {
-        //if(copy_to_user(buf,demoBuffer,count)){
-        //        count=-EFAULT;
-        //}
-        return count;
+	if(copy_to_user(buf,demoBuffer,count)){
+		count=-EFAULT;
+	}
+
+	return count;
 }
 
-size_t simple_write(struct file *file,const char __user *buf,size_t count,loff_t *f_pos)
+ssize_t simple_write(struct file *file,const char __user *buf,size_t count,loff_t *f_pos)
 {
-        //if (copy_from_user(demoBuffer + *f_pos,buf,count)){
-        //        count = -EFAULT;
-        //}
-        return count;
-}
+	if (copy_from_user(demoBuffer + *f_pos,buf,count)){
+		count = -EFAULT;
+	}
 
+	return count;
+}
 
 static const struct file_operations simple_fops={
-        .owner=THIS_MODULE,
-        .open=simple_open,
-        .release=simple_release,
-        //.read=simple_read,
-        //.write=simple_write,
+	.owner=THIS_MODULE,
+	.open=simple_open,
+	.release=simple_release,
+	.read=simple_read,
+	.write=simple_write,
 };
 
 int simple_init_module(void)
 {
-        int ret;
-        struct class *simple_class;
+	int ret;
+	struct class *simple_class;
 
-        memset(demoBuffer,0,256);
-        ret=register_chrdev(simple_MAJOR,"simple",&simple_fops);
-        if(ret<0){
-                printk("Unable to register character device %d!/n",simple_MAJOR);
-                return ret;
-        }
-        simple_class = class_create(THIS_MODULE, "simple_driver");
-        device_create(simple_class, NULL, MKDEV(simple_MAJOR,0), NULL, "simple_device");
+	memset(demoBuffer,0,256);
+	ret=register_chrdev(simple_MAJOR,"simple",&simple_fops);
+	if(ret<0){
+		printk("Unable to register character device %d!/n",simple_MAJOR);
+		return ret;
+	}
 
-        printk("simple_init_module OK!\n");
-        return 0;
+	simple_class = class_create(THIS_MODULE, "simple_driver");
+	device_create(simple_class, NULL, MKDEV(simple_MAJOR,0), NULL, "simple_device");
+
+	printk("simple_init_module OK!\n");
+
+	return 0;
 }
 
 void simple_cleanup_module(void)
 {
-        unregister_chrdev(simple_MAJOR,"simple");
-        printk("simple_cleanup_module!\n");
+	unregister_chrdev(simple_MAJOR,"simple");
+	printk("simple_cleanup_module!\n");
 }
 module_init(simple_init_module);
 module_exit(simple_cleanup_module);
